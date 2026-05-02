b5325717a2c7f278c21faaa902012c2f62bf1f6f
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-03 04:12:30 +0600

    animate css added to banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import { ToastContainer } from "react-toastify";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "animate.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -5,12 +5,17 @@ const Banner = () => {
   return (
     <div className="hero min-h-[30vh] bg-base-200 container mx-auto">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-4xl font-bold">Find Your Next Read</h1>
-          <p className="py-6 text-gray-500">
+        <div className="max-w-md space-y-2">
+          <h1 className="text-4xl font-bold animate__animated animate__fadeInDown animate__slower">
+            Find Your Next Read
+          </h1>
+          <p className="animate__animated animate__fadeInUp animate__delay-1s animate__slower">
             Explore thousands of books and borrow them digitally.
           </p>
-          <Link href="/books" className="btn btn-outline btn-lg">
+          <Link
+            href="/books"
+            className="btn btn-outline btn-lg animate__animated animate__pulse animate__slower animate__infinite"
+          >
             Browse Books
           </Link>
         </div>
