dbe5ed67b2c4cb4fd99b904d70f4730a9092afb0
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-02 17:49:32 +0600

    banner and marque added

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Banner = () => {
-  return <div>banner.</div>;
+  return (
+    <div className="hero min-h-[30vh] bg-base-200 container mx-auto">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">Find Your Next Read</h1>
+          <p className="py-6 text-gray-500">
+            Explore thousands of books and borrow them digitally.
+          </p>
+          <Link href="/books" className="btn btn-outline btn-lg">
+            Browse Books
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/homepage/BookMarquee.jsx
+++ src/components/homepage/BookMarquee.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const BookMarquee = () => {
-  return <div>book marquee.</div>;
+  return (
+    <div className="border border-gray-500 py-1 overflow-hidden container mx-auto">
+      <Marquee
+        pauseOnHover={true}
+        behavior="scroll"
+        direction="left"
+        scrollamount="5"
+      >
+        <strong>New Arrivals:</strong>&nbsp; The Alchemist | Clean Code | Cosmos
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; ***Special
+        Discount on Memberships!***
+      </Marquee>
+    </div>
+  );
 };
 
 export default BookMarquee;
